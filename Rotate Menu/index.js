fe51be348a6a5b6d310eaca923b313a459603492
Pedro Elton <pedroelton@msn.com>
2021-02-11 23:11:21 +0900

    js added

--- Rotate Menu/index.js
+++ Rotate Menu/index.js
@@ -1,1 +1,7 @@
-// add your const
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventlistener('click', () => container.classList.add('show-nav'))
+close.addEventlistener('click', () => container.classList.remove('show-nav'))
+
