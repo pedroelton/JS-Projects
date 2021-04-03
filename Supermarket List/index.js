c443dc45978b422dfc7096cae2ec919447a578d8
Pedro Elton <pedroelton@msn.com>
2021-04-03 16:32:43 +0900

    updates

--- Supermarket List/index.js
+++ Supermarket List/index.js
@@ -1,1 +1,18 @@
-alert ('supermarket')
+var items = []
+
+document.querySelector('button[type=submit]')
+    .addEventListener('click', () => {
+        var productName = document.querySelector('input[name=product-name').value
+        var productPrice = document.querySelector('input[name=product-price').value
+
+        items.push({
+            name: productName,
+            price: productPrice
+        })
+        let productList = document.querySelector('.')
+        items.map(function (al) {
+            
+        })
+        
+        alert(items[0].name)
+})
