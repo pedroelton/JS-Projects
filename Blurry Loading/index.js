c480fc19540b338b77fbb4ab804f265d4ffda3b2
Pedro Elton <pedroelton@msn.com>
2021-04-07 08:05:38 +0900

    First Commit

--- Blurry Loading/index.js
+++ Blurry Loading/index.js
@@ -1,1 +1,24 @@
-// add your const
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+let load = 0
+
+let int = setInterval(blurring, 30)
+
+function blurring() {
+    load++
+    
+    if (load > 99) {
+        clearInterval(int)
+    }
+ //load number
+    loadText.innerHTML = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blue(${scale(load, 0, 100, 30, 0)}px)`
+    
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return((num - in_min)*(out_max - out_min)) / (in_max - in_min) + out_min
+}
+//end of load number
