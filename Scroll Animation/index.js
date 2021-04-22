8070d35f55af02069a1c03a010804be73417399f
Pedro Elton <pedroelton@msn.com>
2021-04-22 10:39:00 +0900

    Scroll Animation

--- Scroll Animation/index.js
+++ Scroll Animation/index.js
@@ -1,1 +1,20 @@
-// add your const
+const content = document.querySelectorAll('.content')
+
+window.addEventListener('scroll', showContent)
+
+// showContent()
+
+function showContent() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    
+    content.forEach(content => {
+        
+        const contentTop = content.getBoundingClientRect().top
+        
+        if (contentTop < triggerBottom) {
+            content.classList.add('show')
+        } else {
+            content.classList.remove('show')
+        }
+    })
+}
